691407c02025a203291bd4434661657b281da3a4
ohoff <ohoffman93@gmail.com>
2020-01-09 22:48:59 +0200

    create results

--- src/Query/InitQuery.java
+++ src/Query/InitQuery.java
@@ -27,38 +27,41 @@ public class InitQuery {
 
     private String query;
     private boolean stemming;
+    private static int numberOfQueries;
+
 
     public InitQuery(String query, boolean stemming) {
         this.query = query;
         this.stemming = stemming;
+
     }
 
     /**
      * this method check if the user send a file or a single query, after the check sent to the relevant method.
      */
-    public void initSearcher(){
+    public void initSearcher() {
         File queries = new File(query);
-        if (queries.exists()){
+        if (queries.exists()) {
             //query is a file
             separateToQueries(queries);
-        }else{
+        } else {
             search(query);
         }
     }
 
 
-
-    private void separateToQueries(File query)  {
+    private void separateToQueries(File query) {
         //open the file and separate the queries
-        String queryWords= "";
-        try{
+        String queryWords = "";
+        try {
             queryWords = new String(Files.readAllBytes(Paths.get(query.getPath())));
-        }  catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         Document html = Jsoup.parse(queryWords);
         Elements elements = html.getElementsByTag("num");
+        this.numberOfQueries = elements.size();
 
         ExecutorService threadPool = newFixedThreadPool(6);
         List<Callable<Object>> todo = new ArrayList<Callable<Object>>();
@@ -66,21 +69,36 @@ public class InitQuery {
 
         for (Element element : elements) {
             String queryToSend = element.getElementsByTag("title").text();
-            String queryNumber = element.text();
-            todo.add(Executors.callable(new Searcher(queryToSend,queryNumber, stemming)));
+            String queryNumber = element.childNode(0).toString();
+            queryNumber = getNumbersFromQuery(queryNumber);
+            todo.add(Executors.callable(new Searcher(queryToSend, queryNumber, stemming)));
             //System.out.println(element.text());
         }
         try {
             List<Future<Object>> answers = threadPool.invokeAll(todo);
-        } catch (InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         threadPool.shutdown();
     }
 
     private void search(String query) {
-        Searcher searcher = new Searcher(query,"0",stemming);
+        Searcher searcher = new Searcher(query, "0", stemming);
         //excute parser and stuff...
 
+    }
+
+    private String getNumbersFromQuery(String queryNumber) {
+        String ans = "";
+        for (int i = 0; i < queryNumber.length(); i++) {
+            if (queryNumber.charAt(i) > 47 && queryNumber.charAt(i) < 58) {
+                ans += queryNumber.charAt(i);
+            }
+        }
+        return ans;
+    }
+
+    public static int getNumberOfQueries() {
+        return numberOfQueries;
     }
 }
--- src/Searcher/Results.java
+++ src/Searcher/Results.java
@@ -1,32 +1,69 @@
 package Searcher;
 
+import Application.ViewModel;
 import Query.DominantEntity;
-
+import Query.InitQuery;
+import javax.management.Query;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class Results {
 
     private static Results results;
+    private static boolean lock;
     private static ArrayList<String> resultsArray;
+    private static BlockingQueue<String> queue;
     private static String queryNumber;
+    private static int queriesCounter;
 
-    private Results(ArrayList<String> results, String queryNumber) {
-        this.resultsArray = results;
-        this.queryNumber = queryNumber;
+    private Results() {
+        queue = new ArrayBlockingQueue(10000);
+        lock = true;
+
     }
 
-    public static Results getResultsInstance(ArrayList<String> resultsArray, String queryNumber){
+    public static Results getResultsInstance(){
         if(results == null){
             synchronized (Results.class) {
                 if(results == null){
-                    results = new Results(resultsArray,queryNumber);
+                    results = new Results();
                 }
             }
         }
         return results;
     }
 
+    public void insertToResultList(ArrayList<String> resultsArray, String queryNumber) throws IOException {
+        if (resultsArray == null || queryNumber== null) {
+            return;
+        }
+        if(lock){
+            lock=false;
+            for(String docName: resultsArray) {
+                queue.add(queryNumber+ " 0"+ docName+ " 0");
+            }
+            lock=true;
+        }
+        if(InitQuery.getNumberOfQueries()==queriesCounter){
+            writeToFile(queue);
+        }
+    }
 
+    private void writeToFile(BlockingQueue<String> queue) throws IOException {
 
+        String[] arr = queue.toArray(new String[queue.size()]);
+        queue.clear();
+        //Arrays.sort(arr);
+        FileWriter writer = new FileWriter(new File(ViewModel.getPathToOutput() +"//results.txt"));
+        for (String str : arr) {
+            writer.write(str + System.lineSeparator());
+        }
+        writer.close();
+    }
 }
